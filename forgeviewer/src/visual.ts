9d9991a14526d17e9791d0245d6b51d1522034ce
Pablovide <73917268+Pablovide@users.noreply.github.com>
2022-05-10 23:17:18 +0200

    forge visual fast n furious implementation

--- forgeviewer/src/visual.ts
+++ forgeviewer/src/visual.ts
@@ -1,28 +1,28 @@
 /*
-*  Power BI Visual CLI
-*
-*  Copyright (c) Microsoft Corporation
-*  All rights reserved.
-*  MIT License
-*
-*  Permission is hereby granted, free of charge, to any person obtaining a copy
-*  of this software and associated documentation files (the ""Software""), to deal
-*  in the Software without restriction, including without limitation the rights
-*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-*  copies of the Software, and to permit persons to whom the Software is
-*  furnished to do so, subject to the following conditions:
-*
-*  The above copyright notice and this permission notice shall be included in
-*  all copies or substantial portions of the Software.
-*
-*  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-*  THE SOFTWARE.
-*/
+ *  Power BI Visual CLI
+ *
+ *  Copyright (c) Microsoft Corporation
+ *  All rights reserved.
+ *  MIT License
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the ""Software""), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
 "use strict";
 
 import "./../style/visual.less";
@@ -36,45 +36,120 @@ import DataView = powerbi.DataView;
 import VisualObjectInstanceEnumerationObject = powerbi.VisualObjectInstanceEnumerationObject;
 
 import { VisualSettings } from "./settings";
+import { utcFormat } from "d3";
 export class Visual implements IVisual {
-    private target: HTMLElement;
-    private updateCount: number;
-    private settings: VisualSettings;
-    private textNode: Text;
+  private TOKEN_ENDPOINT = "https://localhost:44348/token";
+  private DOCUMENT_URN = "urn:adsk.objects:os.object:demo-powerbi-report/box.ipt";
 
-    constructor(options: VisualConstructorOptions) {
-        console.log('Visual constructor', options);
-        this.target = options.element;
-        this.updateCount = 0;
-        if (document) {
-            const new_p: HTMLElement = document.createElement("p");
-            new_p.appendChild(document.createTextNode("Update count:"));
-            const new_em: HTMLElement = document.createElement("em");
-            this.textNode = document.createTextNode(this.updateCount.toString());
-            new_em.appendChild(this.textNode);
-            new_p.appendChild(new_em);
-            this.target.appendChild(new_p);
-        }
-    }
+  private target: HTMLElement;
+  private updateCount: number;
+  private settings: VisualSettings;
+  private textNode: Text;
+  private forge_viewer: Autodesk.Viewing.GuiViewer3D = null;
 
-    public update(options: VisualUpdateOptions) {
-        this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
-        console.log('Visual update', options);
-        if (this.textNode) {
-            this.textNode.textContent = (this.updateCount++).toString();
-        }
-    }
+  constructor(options: VisualConstructorOptions) {
+    console.log("Visual constructor", options);
+    this.target = options.element;
+    this.updateCount = 0;
+    this.target.innerHTML = '<div id="forge-viewer"></div>';
+    this.initForgeViewer();
+  }
 
-    private static parseSettings(dataView: DataView): VisualSettings {
-        return <VisualSettings>VisualSettings.parse(dataView);
-    }
+  private async getToken(): Promise<string> {
+    let token;
+    await fetch(this.TOKEN_ENDPOINT)
+      .then((res) => res.json())
+      .then((data) => (token = data));
+    return `${token.tokenType} ${token.accessToken}`;
+  }
 
-    /**
-     * This function gets called for each of the objects defined in the capabilities files and allows you to select which of the
-     * objects and properties you want to expose to the users in the property pane.
-     *
-     */
-    public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstance[] | VisualObjectInstanceEnumerationObject {
-        return VisualSettings.enumerateObjectInstances(this.settings || VisualSettings.getDefault(), options);
-    }
+  private async initForgeViewer(): Promise<void> {
+    let token = await this.getToken();
+    await this.loadScriptAndStyle();
+
+    Autodesk.Viewing.Document.load(
+      this.DOCUMENT_URN,
+      (doc) => {
+        var viewables: Autodesk.Viewing.BubbleNode = doc
+          .getRoot()
+          .getDefaultGeometry();
+        this.forge_viewer.loadDocumentNode(doc, viewables, {}).then((i) => {
+          this.forge_viewer.addEventListener(
+            Autodesk.Viewing.GEOMETRY_LOADED_EVENT,
+            (res) => {
+              this.forge_viewer.getObjectTree((tree) => {
+                var leaves = [];
+                tree.enumNodeChildren(
+                  tree.getRootId(),
+                  (dbId) => {
+                    if (tree.getChildCount(dbId) === 0) {
+                      leaves.push(dbId);
+                    }
+                  },
+                  true
+                );
+              });
+            }
+          );
+        });
+      },
+      (err) => console.error(err)
+    );
+  }
+
+  private async loadScriptAndStyle(): Promise<void> {
+    return new Promise<void>((reslove, reject) => {
+      let forgeviewerjs = document.createElement("script");
+      forgeviewerjs.src =
+        "https://developer.api.autodesk.com/modelderivative/v2/viewers/viewer3D.js";
+
+      forgeviewerjs.id = "forgeviewerjs";
+      document.body.appendChild(forgeviewerjs);
+
+      forgeviewerjs.onload = () => {
+        console.info("Viewer scripts loaded");
+        let link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href =
+          "https://developer.api.autodesk.com/modelderivative/v2/viewers/style.min.css";
+        link.type = "text/css";
+        link.id = "forgeviewercss";
+        document.body.appendChild(link);
+        console.info("Viewer CSS loaded");
+
+        reslove();
+      };
+
+      forgeviewerjs.onerror = (err) => {
+        console.info("Viewer scripts error:" + err);
+        reject(err);
+      };
+    });
+  }
+
+  public update(options: VisualUpdateOptions) {
+    if (!this.forge_viewer) return;
+
+
+    const dbIds = options.dataViews[0].table.rows.map(r => 
+        <number>r[0].valueOf()
+    );
+
+    this.forge_viewer.showAll();
+    this.forge_viewer.setGhosting(true);
+    this.forge_viewer.isolate(dbIds)
+  }
+
+  private static parseSettings(dataView: DataView): VisualSettings {
+    return VisualSettings.parse(dataView) as VisualSettings;
+  }
+
+  public enumerateObjectInstances(
+    options: EnumerateVisualObjectInstancesOptions
+  ): VisualObjectInstance[] | VisualObjectInstanceEnumerationObject {
+    return VisualSettings.enumerateObjectInstances(
+      this.settings || VisualSettings.getDefault(),
+      options
+    );
+  }
 }
